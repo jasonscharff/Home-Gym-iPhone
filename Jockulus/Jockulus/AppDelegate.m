1378977f55b4998262c25d9b1a3cd5736c6fbfb6
jasonscharff <jasonscharff@gmail.com>
2015-01-17 17:03:52 -0500

    Let's try this.

---
+++ Jockulus/Jockulus/AppDelegate.m
@@ -0,0 +1,224 @@
+//
+//  AppDelegate.m
+//  Home Gym
+//
+//  Created by Jason Scharff on 1/16/15.
+//
+//
+
+#import "AppDelegate.h"
+#import <Spotify/Spotify.h>
+#import <MyoKit/MyoKit.h>
+#import "AFHTTPRequestOperationManager.h"
+
+@interface AppDelegate ()
+
+
+
+
+
+@end
+
+@implementation AppDelegate
+
+
+
+
+- (BOOL)application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    [TLMHub sharedHub];
+    
+    
+    return YES;
+}
+
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    // Saves changes in the application's managed object context before the application terminates.
+    [self saveContext];
+}
+
+#pragma mark - Core Data stack
+
+@synthesize managedObjectContext = _managedObjectContext;
+@synthesize managedObjectModel = _managedObjectModel;
+@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
+
+- (NSURL *)applicationDocumentsDirectory {
+    // The directory the application uses to store the Core Data store file. This code uses a directory named "com.pennapps.gomurmur.Home_Gym" in the application's documents directory.
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+}
+
+- (NSManagedObjectModel *)managedObjectModel {
+    // The managed object model for the application. It is a fatal error for the application not to be able to find and load its model.
+    if (_managedObjectModel != nil) {
+        return _managedObjectModel;
+    }
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"Home_Gym" withExtension:@"momd"];
+    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+    return _managedObjectModel;
+}
+
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator {
+    // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it.
+    if (_persistentStoreCoordinator != nil) {
+        return _persistentStoreCoordinator;
+    }
+    
+    // Create the coordinator and store
+    
+    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
+    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"Home_Gym.sqlite"];
+    NSError *error = nil;
+    NSString *failureReason = @"There was an error creating or loading the application's saved data.";
+    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
+        // Report any error we got.
+        NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+        dict[NSLocalizedDescriptionKey] = @"Failed to initialize the application's saved data";
+        dict[NSLocalizedFailureReasonErrorKey] = failureReason;
+        dict[NSUnderlyingErrorKey] = error;
+        error = [NSError errorWithDomain:@"YOUR_ERROR_DOMAIN" code:9999 userInfo:dict];
+        // Replace this with code to handle the error appropriately.
+        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+        abort();
+    }
+    
+    return _persistentStoreCoordinator;
+}
+
+
+- (NSManagedObjectContext *)managedObjectContext {
+    // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.)
+    if (_managedObjectContext != nil) {
+        return _managedObjectContext;
+    }
+    
+    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
+    if (!coordinator) {
+        return nil;
+    }
+    _managedObjectContext = [[NSManagedObjectContext alloc] init];
+    [_managedObjectContext setPersistentStoreCoordinator:coordinator];
+    return _managedObjectContext;
+}
+
+
+
+
+static NSString * const kClientId = @"2c2e95538e2d46a19ba2cdd910883947";
+static NSString * const kCallbackURL = @"jockulus://callback";
+static NSString * const kTokenSwapServiceURL = @"http://pennapps.gomurmur.com:1234/swap";
+
+
+
+
+// Handle auth callback
+-(BOOL)application:(UIApplication *)application
+           openURL:(NSURL *)url
+ sourceApplication:(NSString *)sourceApplication
+        annotation:(id)annotation {
+    
+    
+    
+    // Ask SPTAuth if the URL given is a Spotify authentication callback
+    
+    if ([[SPTAuth defaultInstance]
+         canHandleURL:url
+         withDeclaredRedirectURL:[NSURL URLWithString:kCallbackURL]]) {
+        
+        // Call the token swap service to get a logged in session
+        
+        
+        
+        [[SPTAuth defaultInstance]
+         handleAuthCallbackWithTriggeredAuthURL:url
+         tokenSwapServiceEndpointAtURL:[NSURL URLWithString:kTokenSwapServiceURL]
+         callback:^(NSError *error, SPTSession *session) {
+             
+             if (error != nil) {
+                 NSLog(@"*** Auth error: %@", error);
+                 return;
+             }
+             
+             //              Call the -playUsingSession: method to play a track
+             self.session = session;
+             
+             
+             [SPTRequest userInformationForUserInSession:session callback:^(NSError *error, SPTUser * object) {
+                 NSString *username = object.canonicalUserName;
+                 [[NSUserDefaults standardUserDefaults] setValue:username forKey:@"username"];
+                 [[NSUserDefaults standardUserDefaults] synchronize];
+                 
+                 
+                 AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+                 NSDictionary *parameters = @{@"username": username};
+                 [manager POST:@"http://pennapps.gomurmur.com/register_user.php" parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                     
+                     
+                     
+                 } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                     NSLog(@"Error: %@", error);
+                     
+                 }];
+                 
+                 
+             }];
+             
+             
+             
+             NSString *storyboardName = @"Main";
+             UIStoryboard *storyboard = [UIStoryboard storyboardWithName:storyboardName bundle: nil];
+             UIViewController * vc = [storyboard instantiateViewControllerWithIdentifier:@"playView"];
+             [(UINavigationController*)self.window.rootViewController pushViewController:vc animated:YES];
+             
+             
+         }];
+        return YES;
+    }
+    
+    return NO;
+}
+
+
+
+
+
+
+#pragma mark - Core Data Saving support
+
+- (void)saveContext {
+    NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
+    if (managedObjectContext != nil) {
+        NSError *error = nil;
+        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
+            // Replace this implementation with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+            abort();
+        }
+    }
+}
+
+@end
---
+++ Jockulus/Jockulus/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Jockulus
+//
+//  Created by Jason Scharff on 1/17/15.
+//  Copyright (c) 2015 Jason Scharff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
